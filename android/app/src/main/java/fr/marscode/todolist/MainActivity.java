5e3c3aa3756808573a7644e65606f5929a4bdad4
SkateZen <mariusmdm123@gmail.com>
2023-08-30 22:01:09 +0200

    Start Ionic ToDoList

---
+++ android/app/src/main/java/fr/marscode/todolist/MainActivity.java
@@ -0,0 +1,5 @@
+package fr.marscode.todolist;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'fr.marscode.todolist',
   appName: 'ToDoList',
   webDir: 'www',
   server: {
